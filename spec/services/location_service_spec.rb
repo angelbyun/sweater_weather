28f9ed5d5a2545035f11191929ab2967e9478f01
Angel Byun <abyun22@turing.edu>
2023-06-11 11:19:00 -0600

    change location to Denver

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe LocationService do
   describe "#get_lat_lon" do
     it "returns the latitude and longitude of a city/state" do
-      location_service = LocationService.new.get_lat_lon("Cincinatti,OH")
+      location_service = LocationService.new.get_lat_lon("Denver,CO")
 
       expect(location_service).to be_a(Hash)
       expect(location_service[:results]).to be_an(Array)
@@ -11,7 +11,7 @@ RSpec.describe LocationService do
       location_data = location_service[:results].first
 
       expect(location_data[:providedLocation]).to be_a(Hash)
-      expect(location_data[:providedLocation][:location]).to eq("Cincinatti,OH")
+      expect(location_data[:providedLocation][:location]).to eq("Denver,CO")
 
       expect(location_data[:locations]).to be_an(Array)
       expect(location_data[:locations].first).to be_a(Hash)
@@ -22,9 +22,9 @@ RSpec.describe LocationService do
       expect(lat_lon).to have_key(:lat)
       expect(lat_lon).to have_key(:lng)
       expect(lat_lon[:lat]).to be_a(Float)
-      expect(lat_lon[:lat]).to eq(39.10713)
+      expect(lat_lon[:lat]).to eq(39.74001)
       expect(lat_lon[:lng]).to be_a(Float)
-      expect(lat_lon[:lng]).to eq(-84.50413)
+      expect(lat_lon[:lng]).to eq(-104.99202)
     end
   end
 end
