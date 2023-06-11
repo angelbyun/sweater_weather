aa065832eadf93fa97a6dcb7882e1e9361d63b39
Angel Byun <abyun22@turing.edu>
2023-06-11 10:17:40 -0600

    testing for LocationService

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -6,6 +6,25 @@ RSpec.describe LocationService do
       location_service = LocationService.new.get_lat_lon("Cincinatti,OH")
 
       expect(location_service).to be_a(Hash)
+      expect(location_service[:results]).to be_an(Array)
+
+      location_data = location_service[:results].first
+
+      expect(location_data[:providedLocation]).to be_a(Hash)
+      expect(location_data[:providedLocation][:location]).to eq("Cincinatti,OH")
+
+      expect(location_data[:locations]).to be_an(Array)
+      expect(location_data[:locations].first).to be_a(Hash)
+      expect(location_data[:locations].first).to have_key(:latLng)
+
+      lat_lon = location_data[:locations].first[:latLng]
+
+      expect(lat_lon).to have_key(:lat)
+      expect(lat_lon).to have_key(:lng)
+      expect(lat_lon[:lat]).to be_a(Float)
+      expect(lat_lon[:lat]).to eq(39.10713)
+      expect(lat_lon[:lng]).to be_a(Float)
+      expect(lat_lon[:lng]).to eq(-84.50413)
     end
   end
 end
