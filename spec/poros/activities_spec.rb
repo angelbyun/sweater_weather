d95e564f33716f21180b081418c491ba9d424fa9
Angel Byun <abyun22@turing.edu>
2023-06-13 12:00:38 -0600

    Attempt to test for activities/activity object poros; ran out of time to actually start TDDing for poros

--- spec/poros/activities_spec.rb
+++ spec/poros/activities_spec.rb
@@ -1,7 +1,7 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Activities do
-#   it "exists and has attributes" do
-#     activity = ActivitiesFacade.new.get_activity
-#   end
-# end
+RSpec.describe Activities do
+  it "exists and has attributes" do
+    
+  end
+end
---
+++ spec/poros/activity_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Activity do
+  it "exists and has attributes" do
+    
+  end
+end
